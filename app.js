35d356445ae1e8abcd0abd9fa278199519c1abb0
Khursheed Abbas <mirzakhursheedabbas14@gmail.com>
2025-04-17 21:01:57 +0530

    done

--- app.js
+++ app.js
@@ -5,5 +5,7 @@ const path=require("path")
 const user=require("./route")
 app.use('/user',user)
 app.use(express.static(path.join(__dirname, 'public')))
-
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
 module.exports.handler=serverless(app)
